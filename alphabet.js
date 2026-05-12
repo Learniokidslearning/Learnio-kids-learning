2d298936c7e366c33b727d2827fb3cfcfca5d92c
Learniokidslearning <learniokidslearning@gmail.com>
2026-05-12 18:58:32 +0530

    Create alphabet.js

---
+++ alphabet.js
@@ -0,0 +1,28 @@
+import { getFirestore, collection, getDocs } 
+from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const db = getFirestore();
+
+async function loadAlphabetVideos() {
+
+    const snapshot = await getDocs(collection(db, "alphabet"));
+
+    let html = "";
+
+    snapshot.forEach((doc) => {
+        const data = doc.data();
+
+        html += `
+            <div class="card">
+                <h3>${data.title}</h3>
+                <video width="300" controls>
+                    <source src="${data.videoUrl}" type="video/mp4">
+                </video>
+            </div>
+        `;
+    });
+
+    document.getElementById("contentArea").innerHTML = html;
+}
+
+loadAlphabetVideos();
